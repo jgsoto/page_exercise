1b46707c769f791d27797422a53477f826aca22d
jgsoto <sotoj9268@gmail.com>
2026-04-09 12:07:17 -0500

    Make the interface responsive

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ login_btn.addEventListener("click", async () => {
     const user = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if (user && password) {
+    if (user === "admin" && password === "12345") {
         window.location.href = "search.html";
     } else {
         alert("Please fill in all the fields");
